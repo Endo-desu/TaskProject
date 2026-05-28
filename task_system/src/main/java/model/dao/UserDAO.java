e13f19f77d1c43b8facdcf0c27395e5579401a56
user <user@PC00072001046>
2026-05-28 17:25:30 +0900

    dao完成

---
+++ task_system/src/main/java/model/dao/UserDAO.java
@@ -0,0 +1,71 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.UserBean;
+/**
+ * @author　篠田
+ */
+public class UserDAO {
+
+	public boolean loginCheck(UserBean userId, UserBean password)
+			throws SQLException, ClassNotFoundException {
+		
+		UserBean user = new UserBean();
+		String sql = "SELECT password FROM m_user WHERE user_id = ? AND password = ?";
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			String userID = user.getUserId();
+			String passWord = user.getPassword();
+
+			pstmt.setString(1, userID);
+			pstmt.setString(2, passWord);
+
+			ResultSet res = pstmt.executeQuery();
+
+			if (res.next()) {
+				return true;
+			} else {
+				/*DO NOTHING*/
+			}
+		}
+		return false;
+	}
+	
+	public List<UserBean> selectAll()
+			throws SQLException, ClassNotFoundException {
+
+		List<UserBean> userList = new ArrayList<UserBean>();
+
+		String sql = "SELECT * FROM m_user";
+
+		// データベースへの接続の取得、更新系SQLステートメントの実行
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				ResultSet res = pstmt.executeQuery()) {
+
+			while (res.next()) {
+
+				String userId = res.getString("user_id");
+				String password = res.getString("password");
+				String userName = res.getString("user_name");
+
+				//リストに入力値を格納
+				UserBean user = new UserBean();
+				user.setUserId(userId);
+				user.setPassword(password);
+				user.setUserName(userName);
+
+				userList.add(user);
+			}
+		}
+		return userList;
+	}
+}
